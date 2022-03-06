816947ec7d7c9dd6ca4c166058cccd74e0300055
josephndegwa <kariukindegwa98@gmail.com>
2022-03-06 15:33:54 +0300

    add following profile viewer logic

--- viewer/views.py
+++ viewer/views.py
@@ -37,3 +37,31 @@ def profile(request, user_id):
 
     return render(request, "profile.html", {"posts": posts, "following": following, "followers": followers, "individual_followers": individual_followers,"current_user": current_user})
 
+
+def profile_follow(request):
+    followed_user = User.objects.get(id=request.POST['id'])
+
+    # if already being followed
+    if Following.objects.filter(follower_id=request.user, following_id=followed_user.id):
+        # get the information for the user's profile
+        followers = Following.objects.filter(following_id=followed_user.id)
+        following = Following.objects.filter(follower_id=followed_user.id)
+        posts = Image.objects.filter(user_id=followed_user.id)
+
+        individual_followers = []
+
+        for follower in followers:
+            individual_followers.append(follower.follower)
+        
+        return redirect("profile", user_id=followed_user.id)
+    else:
+        follow = Following(follower=request.user, following=followed_user) 
+        follow.save()
+
+        # get the information for the user's profile
+        followers = Following.objects.filter(following_id=followed_user.id)
+        following = Following.objects.filter(follower_id=followed_user.id)
+        posts = Image.objects.filter(user_id=followed_user.id)
+
+        return redirect("profile", user_id=followed_user.id)
+
