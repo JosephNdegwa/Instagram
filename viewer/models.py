9c1e8fd03b6079677082a9a132c509f270120764
josephndegwa <kariukindegwa98@gmail.com>
2022-03-07 21:27:04 +0300

    update follower models

--- viewer/models.py
+++ viewer/models.py
@@ -22,3 +22,7 @@ class Profile(models.Model):
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
 
+
+class Following(models.Model):
+    follower = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
