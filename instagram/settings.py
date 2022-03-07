4359295c989add0a903562789ce4502d14736ec3
josephndegwa <kariukindegwa98@gmail.com>
2022-03-07 22:26:17 +0300

    add styles

--- instagram/settings.py
+++ instagram/settings.py
@@ -102,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 
 # Internationalization
