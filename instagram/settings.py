52c71b84c8d6e721990088016d77f795f102e728
josephndegwa <kariukindegwa98@gmail.com>
2022-03-09 01:54:12 +0300

    initial commit

--- instagram/settings.py
+++ instagram/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import dj_database_url
 import os
 import django_heroku
+from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,12 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+MODE=config("MODE", default="dev")
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)ijp=e2*9g$@&k4wn)nc-+z(uv_#7qx3179y6hl4y(%=!vr1v='
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -80,15 +82,29 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
+if config('MODE')=="dev":
+ DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'instagram',
-        'PASSWORD':'515021',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
     }
 }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -126,11 +142,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # configuring the location for media
 MEDIA_URL = '/media/'
--- viewer/tests.py
+++ viewer/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile, Following, Image
 
-# Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.profile = User.objects.create(username="ken")
+    
+    def test_proxy_user(self):
+        self.profile.bio = ""
+        self.assertEqual(self.profile.bio, "")
+
+class FollowingTestCase(TestCase):
+    def setUp(self):
+        self.follower = User.objects.create(username="mike6")
+        self.following = User.objects.create(username="grace")
+        self.following = Following.objects.create(follower=self.follower, following=self.following)
+
+    def test_follow(self):
+        self.assertEqual(self.following.follower, self.follower)
+        self.assertEqual(self.following.following, self.following)
+
+class ImageTestCase(TestCase):
+    def setUp(self):
+        self.image = Image(name="sunset")
+
+    def test_likes(self):
+        self.assertEqual(self.image.get_likes(), 0)
+
+    def test_update_caption(self):
+        self.image.update_caption("nature")
+        self.assertEqual(self.shortDescription, "nature")
