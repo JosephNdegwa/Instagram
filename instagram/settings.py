eb4e61f7694d7bfdcea1f761e0b09957c031e141
josephndegwa <kariukindegwa98@gmail.com>
2022-03-09 03:17:20 +0300

    update settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODE=config("MODE", default="dev")
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
